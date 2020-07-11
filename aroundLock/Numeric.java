d548d3f945b91a71741c47b513728eeff2cdd928
Shilin4397 <Shi_lin4397@163.com>
2020-07-11 16:55:30 +0800

    是否为数值

---
+++ aroundLock/Numeric.java
@@ -0,0 +1,49 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: ShiLin
+ * Date:
+ * Time:
+ */
+public class Numeric {
+    private int index = 0;
+
+    public boolean isNumeric(char[] str) {
+        if (str.length < 1)
+            return false;
+
+        boolean flag = scanInteger(str);
+
+        if (index < str.length && str[index] == '.') {
+            index++;
+            flag = scanUnsignedInteger(str) || flag;
+        }
+
+        if (index < str.length && (str[index] == 'E' || str[index] == 'e')) {
+            index++;
+            flag = flag && scanInteger(str);
+        }
+
+        return flag && index == str.length;
+
+    }
+
+    private boolean scanInteger(char[] str) {
+        if (index < str.length && (str[index] == '+' || str[index] == '-') )
+            index++;
+        return scanUnsignedInteger(str);
+
+    }
+
+    private boolean scanUnsignedInteger(char[] str) {
+        int start = index;
+        while (index < str.length && str[index] >= '0' && str[index] <= '9')
+            index++;
+        return start < index; //是否存在整数
+    }
+
+    public static void main(String[] args) {
+        Numeric n = new Numeric();
+        n.isNumeric("+23.23e2".toCharArray());
+    }
+}
